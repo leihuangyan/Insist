fb54c8f9cafed8d33024832a6e732d47627ee8c1
lhy <15897476550@163.com>
2020-06-28 22:35:56 +0800

    注册中心替换为nacos

--- insist-web/insist-web-control/src/main/java/com/lhy/insist/RunApp.java
+++ insist-web/insist-web-control/src/main/java/com/lhy/insist/RunApp.java
@@ -2,6 +2,7 @@ package com.lhy.insist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -14,7 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @description: RunApp Feign实现  客户端可无需注册
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
 public class RunApp {
